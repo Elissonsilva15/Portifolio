630848102d64d00bf0c7767d38cbaf41cc5ec1a3
ElissonAmaro <222719670+Elissonsilva15@users.noreply.github.com>
2025-08-17 07:16:00 -0300

    atualização
    Refatoração de index.html e scripts.js; remoção de chat.css e atualização de estilos. Layout de seção do projeto aprimorado e responsividade aprimorada. Script atualizado para anexar cartões de projeto dinamicamente.

---
+++ assets/project/Projeto_Noticias/card.js
@@ -0,0 +1,31 @@
+const projetos = document.getElementById('projetos')
+
+const interfaces = {
+    imagem: './assets/project/Projeto_Noticias/img/noticias.png',
+    Projeto: 'Portal de Notícias',
+    tecnologias: ['HTML', 'CSS', 'JavaScript'],
+    data: '16/08/2025',
+}
+card(interfaces)
+
+function card(cards) {
+    const card = document.createElement('div')
+    card.classList.add('card')
+    card.innerHTML = `
+        <img src="${cards.imagem}" alt="${cards.Projeto}" class="img-Portifolio">
+        <h2>${cards.Projeto}</h2>
+        
+        <p>🛠️ Tecnologias Utilizadas <br>${cards.tecnologias}</p>
+        <p>Adicionado em<br>${cards.data}</p>
+    `
+
+    card.addEventListener('click', () => {
+        window.location.href = "assets/project/Projeto_Noticias/sobre/sobre.html";
+    })
+
+    projetos.appendChild(card)
+
+}
+// window.location.href = "assets/project/Projeto_Noticias/sobre/sobre.html";
+
+export default card
--- scripts.js
+++ scripts.js
@@ -1,11 +1,20 @@
-// window.addEventListener('scroll', function () {
-//   const cabecalho = document.getElementById('cabecalho');
-//   if (window.scrollY > 50) {
-//     cabecalho.classList.add('scrolled');
-//   } else {
-//     cabecalho.classList.remove('scrolled');
-//   }
-// });
+import card from './assets/project/Projeto_Noticias/card.js'
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const containerProjetos = document.getElementById('projetos');
+  if (containerProjetos) {
+    containerProjetos.appendChild(novoCard);
+  }
+});
+
+
+
+
+
 
 window.onscroll = function () {
   if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
@@ -14,6 +23,9 @@ window.onscroll = function () {
     document.getElementById("btnsubir").style.display = "none";
   }
 };
+
+
+
 
 const botoes = document.querySelectorAll('.btn-sobre');
 const detalhes = document.querySelectorAll('.detalhes');
@@ -27,11 +39,13 @@ const confirmacao = document.querySelector('.confirmacao')
 
 
 
+
+
 botoes.forEach((btn, index) => {
   btn.addEventListener('click', () => {
     const detalhe = detalhes[index];
 
-    
+
     const estilo = window.getComputedStyle(detalhe);
 
     if (estilo.display === 'none') {
@@ -76,49 +90,7 @@ Enviarbutton.addEventListener('click', () => {
 
 
 
-// const URL = 'https://estudos123456789.app.n8n.cloud/webhook-test/enviar'
-const URL = 'https://estudos123456789.app.n8n.cloud/webhook/enviar'
-
-async function enviarEmail() {
 
 
-  if (inputnome.value.trim() !== '' && inputMensagem.value.trim() !== '') {
-    const envaido = {
-      nome: inputnome.value,
-      mensagem: inputMensagem.value
-    };
 
-    try {
-      Enviarbutton.innerHTML = `Enviando`
-      const enviar = await fetch(URL, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(envaido)
-      });
 
-      Enviarbutton.innerHTML = ` <svg xmlns="http://www.w3.org/2000/svg" width="30px" height="30px" viewBox="0 0 24 24" fill="none">
-        <path d="M7.39999 6.32003L15.89 3.49003C19.7 2.22003 21.77 4.30003 20.51 8.11003L17.68 16.6C15.78 22.31 12.66 22.31 10.76 16.6L9.91999 14.08L7.39999 13.24C1.68999 11.34 1.68999 8.23003 7.39999 6.32003Z" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-        <path d="M10.11 13.6501L13.69 10.0601" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-      </svg>`
-      inputnome.value = ''
-      inputMensagem.value = ``
-
-      chat.style.display = 'none'
-
-      confirmacao.style.display = 'block'
-
-      setTimeout(() => {
-        confirmacao.style.display = 'none'
-      }, 5000)
-
-      const dados = await enviar.json();
-
-      span.textContent = dados.resposta
-    } catch (error) {
-      console.error("Erro ao enviar:", error);
-    }
-  }
-
-}
